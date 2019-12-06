692949113b4bba7013d170fd2f823584d4278f9c
Michael Weedman <michael.weedman@gmail.com>
2019-12-06 12:25:46 -0700

    Clean Up
    Added modules for Authentication and Error handling.  Added URL verification and Rating Verification (I thought this was not a required field).

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const helmet = require('helmet');
 const logger = require('./logger');
 const { NODE_ENV } = require('./config');
 const bookmarksRouter = require('./bookmarks/bookmarks-router');
+const errorHandler = require('./errorHandler');
+const authentication = require('./authentication');
 
 const app = express();
 
@@ -20,30 +22,9 @@ app.use(express.json());
 app.use('/bookmarks', bookmarksRouter);
 
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  console.log(apiToken);
-  const authToken = req.get('Authorization');
-  console.log(authToken);
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-  // move to the next middleware
-  next();
-});
+app.use(authentication);
 
 
 
--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bookmarks = require('../dataStore');
+const { isWebUri } = require('valid-url');
 const uuid = require('uuid/v4');
 const logger = require('../logger');
 
@@ -13,20 +14,26 @@ bookmarksRouter
       .json(bookmarks);
   })
   .post(bodyParser, (req, res) => {
-    const { name, url, rating = '', description = ''} = req.body;
+    const { name, url, rating, description = ''} = req.body;
     console.log(req.body);
 
     if(!name){
       logger.error('Name is required.');
       return res
         .status(400)
-        .send('Required fields: Name and URL');
+        .send('Required fields: Name, Rating (between 0 and 5), and valid URL');
     }
-    if(!url){
-      logger.error('URL is required.');
+    if(!isWebUri(url)){
+      logger.error('Valid URL is required.');
       return res
         .status(400)
-        .send('Required fields: Name and URL');
+        .send('Required fields: Name, Rating (between 0 and 5), and valid URL');
+    }
+    if(!Number.isInteger(rating) || rating < 0 || rating > 5){
+      logger.error('Rating is out of range between 0 and 5.')
+      return res
+        .status(400)
+        .send('Required fields: Name, Rating (between 0 and 5), and valid URL')
     }
 
     const id = uuid();
